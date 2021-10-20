e336a9d40604200650049faff9e517d813086b92
Jurini <curvello_@hotmail.com>
2021-10-20 11:41:04 -0300

    Lista de exercícios 4 - (vogais)

---
+++ vogais.py
@@ -0,0 +1,15 @@
+def vogal(x):
+    if x == 'a' or 'A':
+        return True
+    elif x == 'e' or 'E':
+        return True
+    elif x == 'i' or 'I':
+        return True
+    elif x == 'o' or 'O':
+        return True
+    elif x == 'u' or 'U':
+        return True
+    else:
+        return False
+
+print(vogal('e'))
