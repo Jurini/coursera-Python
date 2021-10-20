d656c39f0033f9b83daf34c274cd449dab9314bc
Jurini <curvello_@hotmail.com>
2021-10-20 15:18:25 -0300

    Refatoração com funções

--- bhaskara.py
+++ bhaskara.py
@@ -1,31 +1,29 @@
 import math
 
-# Nenhuma raiz real: quando delta for menor que zero. (negativo)
-# Uma única raiz real: quando delta for igual a zero. (nulo)
-# Duas raízes reais: quando delta for maior que zero. (positivo)
 
-a = int(input('Informe o valor de a: '))
-b = int(input('Informe o valor de b: '))
-c = int(input('Informe o valor de c: '))
-
-delta = b ** 2 - 4 * a * c
+def delta(a, b, c):
+    return b ** 2 - 4 * a * c
 
 
-def bhaskara_(a, b, delta):
-    return (-b - math.sqrt(delta)) / (2 * a)
+def main():
+    a = int(input('Informe o valor de a: '))
+    b = int(input('Informe o valor de b: '))
+    c = int(input('Informe o valor de c: '))
+    imprime_raizes(a, b, c)
 
 
-def bhaskara(a, b, delta):
-    return (-b + math.sqrt(delta)) / (2 * a)
-
-
-if delta == 0:
-    raiz1 = bhaskara()
-    print('a raiz desta equação é', raiz1)
-else:
-    if delta < 0:
-        print('esta equação não possui raízes reais')
+def imprime_raizes(a, b, c):
+    d = delta(a, b, c)
+    if d == 0:
+        raiz1 = (-b + math.sqrt(d)) / (2 * a)
+        print('a raiz desta equação é', raiz1)
     else:
-        raiz1 = bhaskara()
-        raiz2 = bhaskara_()
-        print('as raízes da equação são', raiz2, 'e', raiz1)
+        if d < 0:
+            print('esta equação não possui raízes reais')
+        else:
+            raiz1 = (-b + math.sqrt(d)) / (2 * a)
+            raiz2 = (-b - math.sqrt(d)) / (2 * a)
+            print('as raízes da equação são', raiz2, 'e', raiz1)
+
+
+main()
