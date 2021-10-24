ecfc2dcc527e380136aaea6e938d556d82ea8e9d
Jurini <curvello_@hotmail.com>
2021-10-24 15:46:47 -0300

    tabuada com laços encaixados 2

--- tabuada.py
+++ tabuada.py
@@ -1,10 +1,12 @@
-linha = 1
-coluna = 1
+def tabuada():
+    tab = 1
+    while tab <= 10:
+        i = 1
+        while i <= 10:
+            print(tab, "x", i, "=", tab * i)
+            i = i + 1
+        print()
+        tab = tab + 1
 
-while linha <= 10:
-    while coluna <= 10:
-        print(linha * coluna, end = "\t")
-        coluna = coluna + 1
-    linha = linha + 1
-    print("")
-    coluna = 1
+
+tabuada()
