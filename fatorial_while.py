ba2bb22f2fb3cd8846c9d20c00fc1421aa8f480e
Jurini <curvello_@hotmail.com>
2021-10-25 20:42:16 -0300

    fatial com laços encaixados

---
+++ fatorial_while.py
@@ -0,0 +1,10 @@
+from math import factorial
+
+n = 0
+
+while n >= 0:
+    while n >= 0:
+        n = int(input('Digite um número e descubra o seu fatorial: '))
+        if n >= 0:
+            f = factorial(n)
+            print(f)
